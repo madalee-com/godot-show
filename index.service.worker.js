9edf7dffb56a1bd2c080b28699cb1e633b71f81a
madalee-com <madalee@ohlmeyers.com>
2026-03-01 20:00:30 +0000

    Deploying to gh-pages from @ madalee-com/godot-show@2f5ecd8148e27dc7c0a03f1740067d5c4a7564c3 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771894632|6846034';
+const CACHE_VERSION = '1772395212|7136331';
 /** @type {string} */
 const CACHE_PREFIX = 'Godot-Show-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
